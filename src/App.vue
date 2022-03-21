12c8fa9e0beb577fea79af0ec510274d1d0c7576
nessun-dorma <vmcringe.asm@gmail.com>
2022-03-21 19:25:31 +0530

    Initial and added a NavBar Component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavigationBar :window-size=this.windowsSize />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavigationBar from "@/components/NavigationBar";
 export default {
   name: 'App',
+  data: function (){
+    return {
+      windowsSize: 0,
+    }
+  },
   components: {
-    HelloWorld
+    NavigationBar
+  },
+  watch: {
+    windowsSize(){
+      console.log(this.windowsSize)
+    }
+  },
+  mounted() {
+    window.addEventListener("resize", () => {
+        this.windowsSize = window.innerWidth
+    })
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav class="main-nav">
+<div class="logo-div">
+  <img alt="Buzzar logo" src="../assets/Logo.svg" class="logo">
+</div>
+  <div class="nav-div">
+    <ul class="nav-list">
+      <li v-if="windowSize > 851"><a href="#">Contact</a></li>
+      <li v-if="windowSize > 851"><a href="#">FAQ</a></li>
+      <li v-if="windowSize > 851"><a href="#">Help</a></li>
+      <li><a href="#">Become a Seller</a></li>
+    </ul>
+  </div>
+  </nav>
+<!--  <div>{{ this.windowSize }}</div>-->
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+  props: ["windowSize"]
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+  .main-nav {
+    display: flex;
+    justify-content: space-between;
+    width: 100vw;
+  }
+
+  .logo-div, .nav-list, li, li a {
+    display: inline-block;
+    text-decoration: none;
+  }
+
+  .logo {
+    padding: 2rem;
+  }
+
+  .nav-list li {
+    list-style: none;
+    margin: 35px 0 0 30px;
+    padding: 15px 20px;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+    color: #99319B;
+  }
+
+  .nav-list li:last-child{
+    background: #99319B;
+    margin-right: 25px;
+    border-radius: 10px;
+  }
+
+  .nav-list li:last-child a {
+    color: white;
+  }
+</style>
